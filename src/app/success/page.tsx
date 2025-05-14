e657d2617e181106dcebe661e42d79b81e142661
yaser-elbatal <yaserelbatal7@gmail.com>
2025-05-14 18:22:36 +0400

    update UI

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
-
 "use client";
 
 import popperAnimation from "@/../public/popper-animation1.json";
@@ -7,28 +5,42 @@ import checkCircleAnimation from "@/../public/success-animation.json";
 import LottieAnimation from "@/components/ui/LottieAnimation";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function SuccessPage() {
   const router = useRouter();
+  const [dimensions, setDimensions] = useState<{
+    width: number;
+    height: number;
+  } | null>(null);
 
   useEffect(() => {
+    // Redirect after 10 seconds
     const timeout = setTimeout(() => {
       router.push("/");
     }, 10000);
+
+    // Set window dimensions
+    setDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
     return () => clearTimeout(timeout);
   }, [router]);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 px-6 relative overflow-hidden">
       <div className="fixed inset-0 z-0 pointer-events-none">
-        <LottieAnimation
-          animationData={popperAnimation}
-          width={window.innerWidth}
-          height={window.innerHeight}
-          loop={false}
-          autoplay={true}
-        />
+        {dimensions && (
+          <LottieAnimation
+            animationData={popperAnimation}
+            width={dimensions.width}
+            height={dimensions.height}
+            loop={false}
+            autoplay={true}
+          />
+        )}
       </div>
       <div className="bg-white p-8 shadow-lg rounded-2xl max-w-xl text-center space-y-6 relative z-10 flex flex-col justify-center items-center">
         <LottieAnimation
@@ -50,7 +62,7 @@ export default function SuccessPage() {
         <motion.button
           key={"2"}
           onClick={() => router.push("/")}
-          className={`${"bg-secondary hover:bg-secondary"} text-white px-6 py-3 rounded-md font-medium transition duration-300`}
+          className="bg-secondary hover:bg-secondary text-white px-6 py-3 rounded-md font-medium transition duration-300"
           variants={{
             hidden: { opacity: 0, y: 20 },
             visible: { opacity: 1, y: 0 },
@@ -58,7 +70,7 @@ export default function SuccessPage() {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
-          {"Go to Home"}
+          Go to Home
         </motion.button>
       </div>
     </div>
